eee0949d7e7200482a9beed431274f38d0a2f659
MattCaleb165 <chm165@live.com>
2023-06-05 19:03:25 -0400

    fixed some functions

--- python_gui_testing/oregon_trail/oregon_trail.py
+++ python_gui_testing/oregon_trail/oregon_trail.py
@@ -1,24 +1,26 @@
 import tkinter as tk
+import random
 
 is_running = False  # Global variable to control the game loop
 
-def game_loop(stop_word):
-    def handle_quit(event):
-        global is_running
-        is_running = False
+is_running = True  # Flag to indicate if the game is running
 
-    # Bind the Quit event to handle_quit function
-    window.bind("<Destroy>", handle_quit)
+def game_loop(stop_word):
+    global is_running
 
     while is_running:
-        # Wait for user input
-        window.wait_variable(user_input)
+        user_input = process_input()  # Get user input from the GUI
 
-        # Get the input value from the shared variable
-        input_value = user_input.get()
+        if user_input.lower() == stop_word.lower():
+            stop_game()  # Call the stop_game function to stop the game
+        else:
+            process_input(user_input)
 
-        # Process user input
-        process_input(input_value, stop_word)
+def stop_game():
+    global is_running
+    is_running = False
+    print_to_gui("Goodbye!")  # Display a farewell message or perform any necessary cleanup tasks
+
 
 def process_input(input_value, stop_word):
     global is_running
@@ -35,11 +37,61 @@ def process_input(input_value, stop_word):
 def start_game(stop_word):
     global is_running
 
-    print_to_gui("Welcome to the Text Adventure Game!\n")
-    print_to_gui("Type '{}' to exit the game.\n".format(stop_word))
+    game_action()
 
     is_running = True  # Start the game loop
     game_loop(stop_word)
+
+player_health = 100  # Initial health value
+def game_action():
+    global player_health
+
+    outcomes = {
+        1: find_treasure_chest,
+        2: encounter_friendly_npc,
+        3: stumble_upon_hidden_passage,
+        4: attack_by_monster,
+        5: nothing_interesting_happens
+    }
+
+    outcome = random.randint(1, 5)
+
+    if outcome in outcomes:
+        action = outcomes[outcome]
+        action()
+
+    if player_health <= 0:
+        print_to_gui("Game over! Your health has reached zero.")
+        stop_game()
+
+def find_treasure_chest():
+    global player_health
+    print_to_gui("You find a treasure chest!")
+    player_health += 10
+    if player_health < 0:
+        player_health = 0
+    print_to_gui("Your health increases by 10. Current health: {}".format(player_health))
+
+def encounter_friendly_npc():
+    global player_health
+    print_to_gui("You encounter a friendly NPC.")
+    # Perform some action that affects the player's health
+
+def stumble_upon_hidden_passage():
+    global player_health
+    print_to_gui("You stumble upon a hidden passage.")
+    # Perform some action that affects the player's health
+
+def attack_by_monster():
+    global player_health
+    print_to_gui("You are attacked by a monster!")
+    player_health -= 20
+    if player_health < 0:
+        player_health = 0
+    print_to_gui("Your health decreases by 20. Current health: {}".format(player_health))
+
+def nothing_interesting_happens():
+    print_to_gui("Nothing interesting happens.")
 
 def print_to_gui(message):
     text_output.config(state="normal")
@@ -78,6 +130,9 @@ input_text.bind("<Return>", process_enter_key)
 stop_word = "quit"  # Specify the stop word
 start_button = tk.Button(window, text="Start Game", command=lambda: start_game(stop_word))
 start_button.pack()
+
+print_to_gui("Welcome to the Text Adventure Game!\n")
+print_to_gui("Type '{}' to exit the game.\n".format(stop_word))
 
 # Redirect stdout to the Text widget
 import sys
